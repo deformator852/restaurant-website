86563c5c649475f99e0ab1ba47bfb7cc29608597
deformator852 <kostneko30@gmail.com>
2025-03-10 05:16:29 -0400

    changes

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
-from .views import Home
+from .views import HomePage, AboutPage
 
-urlpatterns = [path("", Home.as_view())]
+urlpatterns = [path("", HomePage.as_view()), path("about/", AboutPage.as_view())]
--- home/views.py
+++ home/views.py
@@ -3,10 +3,15 @@ from django.views import View
 from .models import Product
 
 
-class Home(View):
+class HomePage(View):
     def get(self, request):
         context = {}
         popular_dishes = Product.objects.all()[0:3].values("name", "image", "price")
         context["popular_dishes"] = popular_dishes
-        print(popular_dishes)
         return render(request, "home/home.html", context=context)
+
+
+class AboutPage(View):
+    def get(self, request):
+        context = {}
+        return render(request, "home/about.html", context=context)
